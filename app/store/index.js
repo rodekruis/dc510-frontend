ab7fe421f0fd7273adc8d6699ba62f527bb75ee0
Madhusudhan Srinivasa <me@madhums.me>
2019-12-15 14:46:11 +0100

    add redux logger

--- app/store/index.js
+++ app/store/index.js
@@ -1,12 +1,13 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import rootReducer from '../reducers';
 import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
+import logger from 'redux-logger';
+import rootReducer from '../reducers';
 
 const navMiddleware = createReactNavigationReduxMiddleware(state => state.nav);
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [...getDefaultMiddleware(), navMiddleware]
+  middleware: [...getDefaultMiddleware(), navMiddleware, logger]
 });
 
 export default store;
