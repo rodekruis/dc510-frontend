46532069c4fe5339de217a876808f7ff7b29d719
Madhusudhan Srinivasa <me@madhums.me>
2019-12-19 02:39:56 +0100

    fetch tasks assigned to user

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -8,15 +11,47 @@ class HomeScreen extends React.Component {
   };
 
   render() {
+    const {
+      data: { loading, allActivities }
+    } = this.props;
     return (
       <View style={styles.container}>
-        <Text>Home screen</Text>
+        <Text>Tasks</Text>
+        {loading && <Text>Loading...</Text>}
+        {(allActivities || []).map(({ name, id }) => (
+          <Text key={id}>
+            {id} - {name}
+          </Text>
+        ))}
       </View>
     );
   }
 }
 
-export default withNavigation(HomeScreen);
+const GET_ASSIGNED_TASKS = gql`
+  query assignedTasks($userId: ID!) {
+    user @client {
+      id @export(as: "userId")
+    }
+    allActivities(where: { assignee: { id: $userId } }) {
+      id
+      name
+    }
+  }
+`;
+
+export default graphql(GET_ASSIGNED_TASKS, {
+  options: {
+    fetchPolicy: 'cache-and-network'
+  }
+})(withNavigation(HomeScreen));
+
+HomeScreen.propTypes = {
+  data: PropTypes.shape({
+    allActivities: PropTypes.array,
+    loading: PropTypes.bool
+  })
+};
 
 const styles = StyleSheet.create({
   container: {
