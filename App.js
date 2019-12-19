a4ab9a3b15c5ce5a836a08e36cf9666746d4f81e
Madhusudhan Srinivasa <me@madhums.me>
2019-12-19 02:37:38 +0100

    wait until state is persisted then load the app

--- App.js
+++ App.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
-import { Provider, connect } from 'react-redux';
-import { createReduxContainer } from 'react-navigation-redux-helpers';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import store, { persistor } from './app/store';
+import { createAppContainer } from 'react-navigation';
+import { ActivityIndicator, StyleSheet, View, Button } from 'react-native';
 import AppNavigator from './app/navigator';
-import apollo from './app/apollo';
 import ErrorBoundary from './app/components/ErrorBoundary';
+import SetupApollo from './app/apollo';
 
-const App = createReduxContainer(AppNavigator);
-const mapStateToProps = state => ({
-  state: state.nav
-});
-const AppWithNavigationState = connect(mapStateToProps)(App);
+const App = createAppContainer(AppNavigator);
 
 export default class Root extends React.Component {
+  state = {
+    client: null,
+    loaded: false
+  };
+
+  async componentDidMount() {
+    const client = await SetupApollo();
+    this.setState({
+      client,
+      loaded: true
+    });
+  }
+
+  clearData = async () => {
+    const { client } = this.state;
+    await client.resetStore();
+  };
+
   render() {
+    const { client, loaded } = this.state;
+    if (!loaded) return <ActivityIndicator />;
     return (
       <ErrorBoundary>
-        <ApolloProvider client={apollo}>
-          <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-              <AppWithNavigationState />
-            </PersistGate>
-          </Provider>
+        <ApolloProvider client={client}>
+          <App />
+          <View style={styles.bottom}>
+            <Button title="Clear data" onPress={this.clearData} />
+          </View>
         </ApolloProvider>
       </ErrorBoundary>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  bottom: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  }
+});
