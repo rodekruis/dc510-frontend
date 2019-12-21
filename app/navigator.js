4a86630bc54955eae9246b7d96bce0c146b087aa
Madhusudhan Srinivasa <me@madhums.me>
2019-12-21 20:03:15 +0100

    add new screens to navigator

--- app/navigator.js
+++ app/navigator.js
@@ -2,13 +2,17 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createSwitchNavigator } from 'react-navigation';
 
 import AuthLoading from './screens/AuthLoading';
+import Login from './screens/Login';
 import Home from './screens/Home';
 import Profile from './screens/Profile';
-import Login from './screens/Login';
+import Task from './screens/Task';
+import AddObservations from './screens/AddObservations';
 
 const AppStack = createStackNavigator({
   Home,
-  Profile
+  Profile,
+  Task,
+  AddObservations
 });
 
 const AuthStack = createStackNavigator({ Login });
